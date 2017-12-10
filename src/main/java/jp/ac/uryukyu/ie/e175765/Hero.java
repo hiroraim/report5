bce4096bb767927aaa7474bded2eef47e7a6dbe8
Hiroto Shimabukuro <e175765@ie.u-ryukyu.ac.jp>
2017-12-11 00:01:36 +0900

    first commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e175765/Hero.java
@@ -0,0 +1,22 @@
+package jp.ac.uryukyu.ie.e175765;
+
+public class Hero extends LivingThing{
+    public Hero (String name, int maximumHP, int attack) {
+        super(name,maximumHP,attack);
+
+    }
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+
+    @Override
+    public void wounded(int damage){
+        setHitPoint(getHitPoint() - damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true) ;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
+        }
+    }
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/e175765/LivingThing.java
@@ -0,0 +1,63 @@
+package jp.ac.uryukyu.ie.e175765;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    //コンストラクタ
+    public LivingThing(String name, int hitPoint, int attack) {
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
+    }
+    //カプセル化
+    //hitPoint's getter
+    public int getHitPoint() {
+        return hitPoint;
+    }
+    //hitPoint's setter
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+
+    //dead's getter
+    public boolean isDead() {
+        return dead;
+    }
+    //dead's setter
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+    //name's getter
+    public String getName() {
+        return name;
+    }
+
+
+
+    public void attack(LivingThing bio){
+        if(!this.dead){
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, bio.getName(), damage);
+            bio.wounded(damage);
+        }
+
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if(hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+
+
+
+}
+
